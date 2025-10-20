2affade536ea247ad080051cd7295ae0e03a4c83
ivan perkov <ivan1996perkov@gmail.com>
2025-10-20 13:51:02 +0300

    i written class Record

--- homework.py
+++ homework.py
@@ -10,14 +10,47 @@ class Field:
 
 class Name(Field):
         pass
-    
-class Phone(Field):
-        pass
+ class Phone(Field):
+          def __init__(self, value):
+            if value is None or not value.strip():
+                  raise ValueError("Phone number dont cannot empty")
+            if not len(value) == 10 and not value.isdigit():
+                  raise ValueError("Phone numbers must be digit")
+            super().__init__(value)
+
 
 class Record:
       def __init__(self, name):
             self.name = Name(name)
             self.phones = []
+
+      def add_phone(self, number):
+            phone = Phone(number)
+            self.phones.append(phone)
+      
+      
+      def remove_phone(self, value):
+           for phone in self.phones:
+                 if phone.value == value:
+                       self.phones.remove(phone)
+           raise ValueError("Phone number not found")
+      
+      def edit_phone(self, old_phone, new_phone):
+            for phone in self.phones:
+                  if phone.value == old_phone:
+                        self.phones.remove(phone)
+                        self.phones.append(Phone(new_phone))
+                        return         
+            raise ValueError("phone not found")
+      
+      def find_phone(self, num):
+            for phone in self.phones:
+                  if phone.value == num:
+                        return phone
+            return None
+                  
+
+      
 
         
       def __str__(self):
@@ -47,3 +80,5 @@ class AdressBook(UserDict):
             return None
       def __str__(self):
             return super().__str__(self.data)
+      
+phone = Phone()
