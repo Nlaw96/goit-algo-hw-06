c984fc51c147e35ea4a1afd7423033dc92a5039d
ivan perkov <ivan1996perkov@gmail.com>
2025-10-20 15:22:19 +0300

    fihish project

--- homework.py
+++ homework.py
@@ -3,18 +3,28 @@ from collections import UserDict
 
 class Field:
     def __init__(self, value):
+        #store the value of the field
         self.value = value
 
     def __str__(self):
+        #return the field value
         return str(self.value)
 
 class Name(Field):
-        pass
- class Phone(Field):
+        # use the constructore for the parent class
+        def __init__(self, value):
+              super().__init__(value)
+
+        def __str__(self):
+              #return name value
+              return super().__str__()
+      
+class Phone(Field):
           def __init__(self, value):
+            #validation phone cannot be empty
             if value is None or not value.strip():
-                  raise ValueError("Phone number dont cannot empty")
-            if not len(value) == 10 and not value.isdigit():
+                  raise ValueError("Phone number  cannot be empty")
+            if not len(value) == 10 or not value.isdigit():
                   raise ValueError("Phone numbers must be digit")
             super().__init__(value)
 
@@ -24,17 +34,20 @@ class Record:
             self.name = Name(name)
             self.phones = []
 
+      #add new phone namber
       def add_phone(self, number):
             phone = Phone(number)
             self.phones.append(phone)
       
-      
+      #remove phone number
       def remove_phone(self, value):
            for phone in self.phones:
                  if phone.value == value:
                        self.phones.remove(phone)
+                       return
            raise ValueError("Phone number not found")
       
+      #replace an old number with a new one
       def edit_phone(self, old_phone, new_phone):
             for phone in self.phones:
                   if phone.value == old_phone:
@@ -43,42 +56,71 @@ class Record:
                         return         
             raise ValueError("phone not found")
       
+      #find a phone in the contact
       def find_phone(self, num):
             for phone in self.phones:
                   if phone.value == num:
                         return phone
             return None
-                  
-
-      
-
-        
+      #formated output for the contact
       def __str__(self):
-            return f"Contact name: {self.name.value}, phone: {";".join(p.value for p in self.phones)}"
-
-
-class AdressBook(UserDict):
+            return f"Contact name: {self.name.value}, phone: {';'.join(p.value for p in self.phones)}"
+      
+#---------    
+#class for the address book
+#---------
+class AddressBook(UserDict):
       def add_record(self, record):
             self.data[record.name.value] = record
 
 
-            
-      def find(self, items):
-            if items in self.data.keys():
-                  return self.data[items]
-            for row in self.data.values():
-                  for phone in row.phones:
-                        if phone == items:
-                              return row
-            return None
+      #find the record by name   
+      def find(self, name):
+            if name not in  self.data:
+                  return None
+            return self.data.get(name)
             
 
-      
+      #delete a record by name
       def delete(self, name):
-            if name in self.data.keys():
-                  self.data.pop(name)
-            return None
+            self.data.pop(name, None)
+
+      #formatted record for the addressbook
       def __str__(self):
-            return super().__str__(self.data)
+            if not self.data:
+                  return "AddressBook is empty"
+            records = ["  " + str(record) for record in self.data.values()]
+            return "AddressBook:\n" + "\n".join(records) 
       
-phone = Phone()
+#create addressbook  
+book = AddressBook()
+
+ # creatr record for  John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+    # Added record for John to address book
+book.add_record(john_record)
+
+    # create and recording a new record for  Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+    # print all record in a book
+     
+print(book)
+
+    # find and edit phone numbers for  John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Print: Contact name: John, phones: 1112223333; 5555555555
+
+    # Search specific record in John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+    # Delete Jane
+book.delete("Jane")
